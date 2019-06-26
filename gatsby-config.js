837c290dff8b0f5858d623a9ac397b56586cb548
im-yeobi <dev.jongyeobKim@gmail.com>
2019-06-26 10:42:22 +0900

    ⚙️ Conf: Update, blog name 'im-yeobi'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `yeobi.io`,
+    title: `im-yeobi.io`,
     author: `Jongyeob Kim`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
