e9edee491513e0e777b5bfd231aaee3acf81d5f4
im-yeobi <dev.jongyeobKim@gmail.com>
2019-12-14 10:52:51 +0900

    ✈️  Update: Side bar 메뉴명 및 타이틀 변경

--- src/templates/categories-list-template.js
+++ src/templates/categories-list-template.js
@@ -12,9 +12,9 @@ const CategoriesListTemplate = () => {
   const categories = useCategoriesList();
 
   return (
-    <Layout title={`Categories - ${title}`} description={subtitle}>
+    <Layout title={`Category - ${title}`} description={subtitle}>
       <Sidebar />
-      <Page title="Categories">
+      <Page title="Category">
         <ul>
           {categories.map((category) => (
             <li key={category.fieldValue}>
